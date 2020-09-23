18d7b7c21f64b1693dab3ff89644b6de9384a8f6
tolookme <tolookme@sina.cn>
2020-09-23 08:10:52 -0700

    add other log

--- main.go
+++ main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello git!")
+
+	fmt.Println("")
+
+	fmt.Println("git status")
+	fmt.Println("git log")
+
+	fmt.Println("")
+
+	fmt.Println("git add main.go")
+	fmt.Println("git commit -m \"add main.go\"")
+	fmt.Println("git push")
+
+	fmt.Println("")
+
+	fmt.Println("git commit -m \"modify main.go\"")
+	fmt.Println("git push")
 }
